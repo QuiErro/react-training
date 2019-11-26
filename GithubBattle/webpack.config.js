2302adbdc18e2e3470a354330f598db68bf676bb
zoek <wioblock124@163.com>
2019-11-26 17:49:50 +0800

    工程化github热门项目

---
+++ GithubBattle/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.join(__dirname, './src/index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: path.join(__dirname, './index.html'),
+            filename: 'index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {test: /\.js$/, use: 'babel-loader'}, 
+            {test: /\.css$/, use: ['style-loader', 'css-loader']},
+            {test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader']},
+            {test: /\.(eot|svg|ttf|woff|woff2|png)\w*/, use: 'file-loader'}
+        ]
+    },
+    mode: 'development' 
+};
